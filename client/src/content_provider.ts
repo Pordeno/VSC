c080d817d9960fb2fb0956612e6d426c551bb67d
PhoneDroid <73050054+PhoneDroid@users.noreply.github.com>
2023-07-15 15:28:48 -0400

    General refactoring

--- client/src/content_provider.ts
+++ client/src/content_provider.ts
@@ -1,33 +1,33 @@
 // Copyright 2018-2022 the Deno authors. All rights reserved. MIT license.
 
-import { virtualTextDocument } from "./lsp_extensions";
-import type { DenoExtensionContext } from "./types";
+export { DenoTextDocumentContentProvider , SCHEME }
 
-import type {
-  CancellationToken,
-  ProviderResult,
-  TextDocumentContentProvider,
-  Uri,
-} from "vscode";
+import type { TextDocumentContentProvider , CancellationToken , Uri } from 'vscode'
+import type { DenoExtensionContext } from './types'
+import { virtualTextDocument } from './lsp_extensions'
 
-export const SCHEME = "deno";
 
-export class DenoTextDocumentContentProvider
-  implements TextDocumentContentProvider {
-  constructor(private extensionContext: DenoExtensionContext) {}
+const SCHEME = 'deno'
 
-  provideTextDocumentContent(
-    uri: Uri,
-    token: CancellationToken,
-  ): ProviderResult<string> {
-    if (!this.extensionContext.client) {
-      throw new Error("Deno language server has not started.");
+
+class DenoTextDocumentContentProvider
+implements TextDocumentContentProvider {
+
+    constructor (
+        private extensionContext : DenoExtensionContext
+    ){}
+
+    provideTextDocumentContent (
+        uri : Uri ,
+        token : CancellationToken
+    ){
+
+        if( ! this.extensionContext.client )
+            throw new Error(`Deno language server has not started.`)
+
+        return this.extensionContext.client
+            .sendRequest(virtualTextDocument,{
+                textDocument : { uri : uri.toString() } } ,
+                token )
     }
-
-    return this.extensionContext.client.sendRequest(
-      virtualTextDocument,
-      { textDocument: { uri: uri.toString() } },
-      token,
-    );
-  }
 }
